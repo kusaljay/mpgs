960035cf7e7efd0e7e6d7af9c851ba6d345ec705
da_kfernando <da_kfernando@ahpra.gov.au>
2019-04-02 19:03:30 +1100

    fix login heading

--- src/components/Login.js
+++ src/components/Login.js
@@ -31,7 +31,7 @@ class Login extends Component {
     return (
       <div className="container login-wrapper d-flex justify-content-center align-items-center">
         <img src={logo} className="logo login" alt="myob logo" />
-        <h1 className="psg-heading-main mb-4">Sign in to PSG</h1>
+        <h1 className="psg-heading-main mb-4">Sign in to Pay Slip Generator</h1>
         <div className="card content-container login">
           <form onSubmit={this.handleSignIn}>
             {this.state && this.state.formErrors && <div className="alert alert-danger form-alerts">{this.state.formErrors}</div>}
